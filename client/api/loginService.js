58bd2b46e085f214a264b2f7ca1cb9dc0f84dedb
L. Filip Kovačić <lfkovacic@gmail.com>
2023-03-24 19:11:54 +0100

    Postavljeni testni APIi /api/user i /api/testtest

--- client/api/loginService.js
+++ client/api/loginService.js
@@ -1,7 +1,10 @@
 export const loginService = {
     getUserData: async () => {
-        const response = { username: "pperic", password: "perozdero123" }
+        const response = { username: "pperic", password: "perozdero123" };
+        const response2 = ["kruška","jabuka", "šljiva"];
+        response2[0];
         return response;
+        //re
     },
     getToken: () => {
         //TODO: AJAX
--- php/controller/ApiController.php
+++ php/controller/ApiController.php
@@ -1,10 +1,20 @@
-if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_SERVER['REQUEST_URI'] == '/api/echoecho') {
-    $response = 'Hello world!';
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_SERVER['REQUEST_URI'] == '/api/users') {
+    // Get list of users from your service or model layer
+    $response = "echoecho";
+    
+    // Return JSON response
     header('Content-Type: application/json');
-    echo json_encode($response)
+    echo json_encode($response);
 }
-if (!isset($_SERVER['REQUEST_URI'] || !isset($_SERVER['REQUEST_METHOD'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_SERVER['REQUEST_URI'] == '/api/test') {
+    $response = json_encode(array('key'=>"value"));
+    echo $response;
+
+}
+if (!isset($_SERVER['REQUEST_URI']) || !isset($_SERVER['REQUEST_METHOD'])) {
     http_response_code(404);
     echo "404 Not Found";
     exit;
 }
+?>
