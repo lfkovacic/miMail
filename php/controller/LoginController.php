a4c2e3b2260c2a2e260478cfb85c1222be77ea0d
L. Filip Kovačić <lfkovacic@gmail.com>
2023-04-28 17:56:53 +0200

    Endpoint - registerUser, dorade na servisima, dorade repository

--- php/controller/LoginController.php
+++ php/controller/LoginController.php
@@ -1,6 +1,9 @@
 <?php
 $rootDirectory = $_SERVER['DOCUMENT_ROOT'];
 include_once($rootDirectory.'/php/Endpoint.php');
+include_once($rootDirectory.'/php/service/UserService.php');
+
+
 
 class EchoUser extends Endpoint {
     protected function parseRequest(&$dto){
@@ -18,4 +21,41 @@ class EchoUser extends Endpoint {
         $data = array('username' => $dto->username, 'password' => $dto->password);
         echo json_encode($data);
 	}
+}
+
+class RegisterUser extends Endpoint {
+    protected function parseRequest(&$dto) {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        $dto->username = $data['username'];
+        $dto->password = $data['password'];
+    }
+
+    protected function execute($dto){
+        header('Content-Type: application/json');
+        $data = array('username' => $dto->username, 'password' => $dto->password);
+        echo json_encode($data);
+    }
+}
+
+class AuthenticateUser extends Endpoint {
+    
+    protected function parseRequest(&$dto) {
+        // Get JSON object from request body
+        $json = file_get_contents('php://input');
+
+        // Decode JSON object into associative array
+        $data = json_decode($json, true);
+        
+
+        // Assign properties to DTO object
+        $dto->username = $data['username'];
+        $dto->password = $data['password'];
+    }
+
+    protected function execute($dto) {
+        $userService = new UserService();
+        header('Content-Type: application/json');
+        echo json_encode($userService->authenticateUser($dto->username, $dto->password));
+    }
 }
