5044beac63a91df926109df4f85d878b6b7c4db9
L. Filip Kovačić <lfkovacic@gmail.com>
2023-05-05 19:54:16 +0200

    Dodana provjera kod registracije korisnika

--- php/service/UserService.php
+++ php/service/UserService.php
@@ -2,22 +2,33 @@
 $rootDirectory = $_SERVER['DOCUMENT_ROOT'];
 include_once($rootDirectory . "/php/repository/UserRepository.php");
 
-class UserService {
+class UserService
+{
     private $userRepository;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->userRepository = new UserRepository();
     }
 
-    public function authenticateUser($username, $password) {
+    public function authenticateUser($username, $password)
+    {
         $user = $this->userRepository->getUser($username);
-        if ($user[0]["USERNAME"] == $username && $user[0]["PWD_HASH"] == $password){
+        if ($user[0]["USERNAME"] == $username && $user[0]["PWD_HASH"] == $password) {
             return true;
         } else return false;
     }
 
-    public function registerUser($username, $pwd_hash){
-        $sql_response = $this->userRepository->registerUser($username, $pwd_hash);
-        return $sql_response;
+    public function registerUser($username, $pwd_hash)
+    {
+        $user = $this->userRepository->getUser($username);
+        if ($user[0]["USERNAME"] == $username)
+            return "Greška kod registracije: korisničko ime " . $username . " već postoji!";
+        else
+            $sql_response = $this->userRepository->registerUser($username, $pwd_hash);
+        if ($sql_response == 1)
+            return "Korisnik uspješno registriran.";
+        else
+            return "Greška kod registracije korisnika!";
     }
 }
